9dbac77e5d3e17dd2bfb5bb05f50c1a0f2ea60a1
jakehl <jake@twilsoft.uk>
2024-03-09 01:49:29 +0000

    small unherd changes

--- cmd/unherd.go
+++ cmd/unherd.go
@@ -15,27 +15,27 @@ func Unherd(repo *repo.DotRepo) *cli.Command {
 			cli.ShowSubcommandHelpAndExit(c, 1)
 		}
 
-		dotLink := c.Args().First()
-		dotLinkAbs, absErr := filepath.Abs(dotLink)
+		link := c.Args().First()
+		linkAbs, absErr := filepath.Abs(link)
 		if absErr != nil {
 			return cli.Exit(fmt.Sprintf("Error getting absolute path: %s", absErr), 1)
 		}
 
-		var nameInPen, nameErr = repo.NameFromLink(dotLinkAbs)
+		var nameInPen, nameErr = repo.NameFromLink(linkAbs)
 		if nameErr != nil {
-			return cli.Exit(fmt.Sprintf("Unable to find alias in pen for dotfile %s.", dotLinkAbs), 1)
+			return cli.Exit(fmt.Sprintf("Unable to find alias in pen for dotfile %s.", linkAbs), 1)
 		}
 
-		if err := os.Remove(dotLinkAbs); err != nil {
+		if err := os.Remove(linkAbs); err != nil {
 			return cli.Exit(fmt.Sprintf("Error removing symlink: %s", err), 1)
 		}
 
-		if err := os.Rename(nameInPen, dotLinkAbs); err != nil {
+		if err := os.Rename(nameInPen, linkAbs); err != nil {
 			fmt.Println(err)
-			return cli.Exit(fmt.Sprintf("Error moving %s to to original location: %s", nameInPen, dotLinkAbs), 1)
+			return cli.Exit(fmt.Sprintf("Error moving %s to to original location: %s", nameInPen, linkAbs), 1)
 		}
 
-		if err := repo.Unherd(dotLinkAbs); err != nil {
+		if err := repo.Unherd(linkAbs); err != nil {
 			return cli.Exit(fmt.Sprintf("Error removing dotfile from repo: %s", err), 1)
 		}
 
@@ -44,8 +44,7 @@ func Unherd(repo *repo.DotRepo) *cli.Command {
 
 	return &cli.Command{
 		Name:     "unherd",
-		Category: "Dotfiles",
-		Usage:    "Unherd a dotfile from the pen (Rerepos the original file)",
+		Usage:    "dot unherd <dotfile>",
 		Action:   unherdAction,
 	}
 }
